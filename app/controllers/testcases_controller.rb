dafe9569e2b7b2ff95dd7c739c2b185d49aefef4
github username <github email address>
2018-01-27 15:51:02 -0500

    add testcases index controller action

--- app/controllers/testcases_controller.rb
+++ app/controllers/testcases_controller.rb
@@ -1,2 +1,12 @@
 class TestcasesController < ApplicationController
+
+  get '/testcases' do
+    if logged_in?
+      @testcases = Testcase.all
+      erb :'/testcases/index'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
