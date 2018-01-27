52a8c80ccfdbff3de25c0dbf680124e0a76b20d5
github username <github email address>
2018-01-27 15:28:48 -0500

    add show feature controller action

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -3,9 +3,18 @@ class FeaturesController < ApplicationController
   get '/features' do
     if logged_in?
       @features = Feature.all
-      erb :'features/features'
+      erb :'features/index'
     else
       redirect to '/login'
+    end
+
+    get '/features/:id' do
+      if logged_in?
+        @feature = Feature.find(params[:id])
+        erb :'features/show'
+      else
+        redirect to '/login'
+      end
     end
   end
 end
