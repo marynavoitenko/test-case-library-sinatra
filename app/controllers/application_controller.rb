b63275d5279cd1d04654045a967a10b9a35a7b95
github username <github email address>
2018-01-27 20:13:19 -0500

    do not show login/signup page to logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    if !logged_in?
+      erb :index
+    else
+      redirect to '/features'
+    end
   end
 
   helpers do
